c2b3c7b928178e1f3d8d8b026d86b8cf69b71bda
Devesh Kumar <devesh2027@gmail.com>
2018-09-26 23:51:55 +0530

    Create style.php

---
+++ inc/style.php
@@ -0,0 +1,11 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<link rel="stylesheet" type="text/css" href="style/style.css">
+<link rel="stylesheet" type="text/css" media="screen and (max-width: 950px)" href="style/smallscreen.css">
+<link rel="stylesheet" type="text/css" media="screen and (max-width: 600px)" href="style/ultrasmall.css">
+
+<!-- Font Awesome Integration -->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+<!-- Roboto -->
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
